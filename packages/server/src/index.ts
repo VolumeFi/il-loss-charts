099146db0711d76ca2476215536b9118481883e5
rango527 <web.mobile497373@gmail.com>
2021-06-25 03:44:00 +0800

    create app.yaml, .gcloudignore

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,12 @@ import { calculateMarketStats } from 'util/calculate-stats';
 import { UniswapV3Fetcher } from 'services/uniswap-v3/fetcher';
 import UniswapFetcher from 'services/uniswap';
 import BitqueryFetcher from 'services/bitquery/fetcher';
+<<<<<<< HEAD
 import { _getBitqueryPeriodIndicators as _getPeriodIndicators } from 'api/controllers/market-data';
+=======
+// import { _getPeriodIndicators } from 'api/controllers/market-data';
+import _getPeriodIndicators from 'api/controllers/market-data';
+>>>>>>> 3a9461e (create app.yaml, .gcloudignore)
 
 export {
     _getPeriodIndicators,
